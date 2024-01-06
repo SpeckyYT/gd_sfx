1913fb04b3b39b665a3539c4dcf1e092d003d543
kr8gz <47776344+kr8gz@users.noreply.github.com>
2024-01-06 19:26:52 +0100

    wtf this didnt work before but whatever

--- build.rs
+++ build.rs
@@ -18,19 +18,14 @@ fn main() {
     let default_lang = serde_json::from_str::<Value>(include_str!(default_lang_path!())).unwrap();
     let default_lang = default_lang.as_object().unwrap().clone();
 
-    // inner blocks for multiple mutable borrows of lang_schema
-    {
-        let properties = lang_schema["properties"].as_object_mut().unwrap();
-        for key in default_lang.keys() {
-            properties.insert(key.to_string(), json!({ "type": "string" }));
-        }
+    let properties = lang_schema["properties"].as_object_mut().unwrap();
+    for key in default_lang.keys() {
+        properties.insert(key.to_string(), json!({ "type": "string" }));
     }
 
-    {
-        let required = lang_schema["required"].as_array_mut().unwrap();
-        for key in default_lang.keys() {
-            required.push(json!(key));
-        }
+    let required = lang_schema["required"].as_array_mut().unwrap();
+    for key in default_lang.keys() {
+        required.push(json!(key));
     }
 
     let json_string = lang_schema.to_string();
