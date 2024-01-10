81b57b11bf8eec769e56e6d93c3d796b9fb3c1ea
kr8gz <47776344+kr8gz@users.noreply.github.com>
2024-01-10 16:28:52 +0100

    ok i can start refactoring attempt now

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -16,31 +16,31 @@ pub const TOTAL_HEIGHT: f32 = 600.0;
 lazy_static!{
     pub static ref GD_FOLDER: PathBuf = {
         if cfg!(target_os = "windows") {
-            PathBuf::from(env::var("localappdata").expect("No local app data"))
+            PathBuf::from(env::var("localappdata").expect("No local app data directory"))
                 .join("GeometryDash")
-        } else if cfg!(target_os = "macos") {
+        }
+        else if cfg!(target_os = "macos") {
             PathBuf::from(env::var("HOME").expect("No home directory"))
                 .join("Library/Application Support/GeometryDash")
-        } else if cfg!(target_os = "linux") {
-            let home_path_str: String = env::var("HOME").expect("home directory ENV variable not set");
-            let home_path: &Path = Path::new(home_path_str.as_str());
+        }
+        else if cfg!(target_os = "linux") {
+            let home_path = env::var("HOME").expect("home directory ENV variable not set");
+            let home_path = Path::new(&home_path);
 
-            let possible_gd_paths: [&str; 2] = [
+            let possible_gd_paths = [
                 ".steam/steam/steamapps/compatiata/322170/drive_c/users/steamuser/Local Settings/Application Data/GeometryDash",
                 "PortWINE/PortProton/prefixes/DEFAULT/drive_c/users/steamuser/AppData/Local/GeometryDash"
             ];
 
-            for path in possible_gd_paths {
-                let full_path: PathBuf = home_path.join(path);
-                if full_path.exists() {
-                    return full_path;
-                }
-            }
-
-            panic!("no GD path found");
-        } else if cfg!(target_os = "android") {
+            possible_gd_paths.iter()
+                .map(|path| home_path.join(path))
+                .find(|path| path.exists())
+                .expect("No GD path found")
+        }
+        else if cfg!(target_os = "android") {
             PathBuf::from("/data/data/com.robtopx.geometryjump")
-        } else {
+        }
+        else {
             panic!("Unsupported operating system");
         }
     };
