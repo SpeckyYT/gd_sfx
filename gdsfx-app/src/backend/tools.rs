15d0579bf12733bd2b7c0adb23e3cf3791e6ba9c
kr8gz <47776344+kr8gz@users.noreply.github.com>
2024-01-15 21:32:51 +0100

    works again

--- gdsfx-app/src/backend/tools.rs
+++ gdsfx-app/src/backend/tools.rs
@@ -1,47 +1,24 @@
-use std::{sync::Arc, thread::spawn};
+use std::{sync::Arc, thread};
 
 use eframe::epaint::mutex::Mutex;
-use gdsfx_library::{LibraryEntry, EntryKind};
-use crate::Library;
+use gdsfx_library::{Library, LibraryEntry, EntryKind};
 
 use super::AppState;
 
-type STATS = Arc<Mutex<Option<(u128, u128)>>>;
+type Stats = Arc<Mutex<Option<(u128, u128)>>>;
 
-pub fn download_all(library: Library, app_state: &AppState, stats: STATS) {
-    let root = library.lock().get_root();
-
+pub fn download_all(app_state: Arc<Mutex<AppState>>, library: Arc<Mutex<Library>>, stats: Stats) {
     *stats.lock() = Some((0, library.lock().get_total_entries() as u128));
 
-    fn recursive<'a>(library: Library, entries: impl Iterator<Item = &'a LibraryEntry>, app_state: &AppState, stats: STATS) {
-        entries.for_each(|entry| {
-            match entry.kind {
-                EntryKind::Category =>
-                    recursive(
-                        library.clone(),
-                        library.lock().get_children(entry),
-                        app_state,
-                        stats.clone(),
-                    ),
-                EntryKind::Sound { .. } => {
-                    app_state.download_sound(entry);
-                    let mut lock = stats.lock();
-                    let previous = *lock;
-                    if let Some(previous) = previous {
-                        *lock = Some((previous.0, previous.1 + 1));
-                    }
-                },
-            };
-        })
-    }
+    let library = library.clone();
     
-    spawn(move || {
-        recursive(
-            library,
-            library.lock().get_children(root),
-            app_state,
-            stats.clone(),
-        );
+    thread::spawn(move || {
+        for sound in library.lock().get_all_sounds() {
+            app_state.lock().download_sound(sound);
+            if let Some(ref mut stats) = *stats.lock() {
+                *stats = (stats.0, stats.1 + 1);
+            }
+        }
 
         *stats.lock() = None;
     });
