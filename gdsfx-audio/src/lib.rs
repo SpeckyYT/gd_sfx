e35687f17d4fc8b1461883ec1478b6e79fffacdb
kr8gz <47776344+kr8gz@users.noreply.github.com>
2024-01-21 00:43:43 +0100

    ok

--- gdsfx-audio/src/lib.rs
+++ gdsfx-audio/src/lib.rs
@@ -1,169 +1,206 @@
-use std::{
-    io::Cursor,
-    sync::Arc,
-    time::Instant,
-};
+use std::{sync::Arc, thread, mem};
 
-use crossbeam_channel::{Receiver, Sender};
-use lazy_static::lazy_static;
-use parking_lot::Mutex;
-use rodio::{Decoder, OutputStream, Sink};
-use kittyaudio::{Frame, Mixer, interpolate_frame, Sound};
+use educe::Educe;
+use libfmod::*;
+use parking_lot::RwLock;
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+type Result<T> = std::result::Result<T, libfmod::Error>;
+
+pub struct AudioSystem {
+    /// The FMOD system object to be used internally.
+    system: System,
+
+    /// The FMOD channel which is currently playing sound, or `None` if no sound is being played.
+    channel: Arc<RwLock<Option<Channel>>>,
+
+    /// Public instance of `AudioSettings` that can be modified at any time.
+    /// 
+    /// Only specific changes to this struct can be applied immediately while playing audio though;
+    /// see the fields of the `AudioSettings` struct for more information.
+    pub settings: Arc<RwLock<AudioSettings>>,
+}
+
+#[derive(Educe, Debug, Clone, Copy, PartialEq)]
+#[educe(Default)]
 pub struct AudioSettings {
-    pub speed: f32, // -12..=12
-    pub pitch: f32, // -12..=12
-    pub volume: f32, // 0..=2
+    /// Exponential speed factor scale from `-12` to `12` (inclusive).
+    /// `-12` represents half speed, while `-12` is double speed.
+    /// 
+    /// Also distorts pitch by this many semitones.
+    /// 
+    /// Modifiable while playing.
+    #[educe(Default = 0)]
+    pub speed: i32,
+
+    /// Semitone offset from `-12` to `12` (inclusive).
+    #[educe(Default = 0)]
+    pub pitch: i32,
+
+    /// Volume range from `0.0` (no sound) to `2.0` (twice as loud).
+    /// 
+    /// Modifiable while playing.
+    #[educe(Default = 1.0)]
+    pub volume: f32,
+
+    /// Whether to loop the sound.
+    /// Takes into account start and end settings, as well as fading in once.
+    /// 
+    /// Can be set to false while playing to stop looping.
+    #[educe(Default = false)]
+    pub looped: bool,
+
+    /// Start offset in milliseconds, before applying the speed modifier.
+    #[educe(Default = 0)]
+    pub start: u32,
+
+    /// End point in milliseconds, before applying the speed modifier.
+    #[educe(Default = 0)]
+    pub end: u32,
+
+    /// Fade-in time in milliseconds, after applying the speed modifier.
+    #[educe(Default = 0)]
+    pub fade_in: u32,
+
+    /// Fade-out time in milliseconds, after applying the speed modifier.
+    #[educe(Default = 0)]
+    pub fade_out: u32,
 }
 
-impl Default for AudioSettings {
-    fn default() -> Self {
-        Self {
-            speed: 0.0,
-            pitch: 0.0,
-            volume: 1.0,
+impl AudioSystem {
+    pub fn new() -> Result<Self> {
+        let system = System::create()?;
+    
+        // Since previously playing audio is stopped when new audio should be played,
+        // a maximum of 1 channel should be enough.
+        system.init(1, Init::NORMAL, None)?;
+
+        Ok(Self {
+            system,
+            channel: Default::default(),
+            settings: Default::default(),
+        })
+    }
+
+    pub fn play_sound(&mut self, bytes: impl AsRef<[u8]>) -> Result<()> {
+        self.stop_audio()?;
+
+        let settings = *self.settings.read();
+
+        let mode = if settings.looped { Mode::LOOP_NORMAL } else { Mode::DEFAULT };
+
+        // SAFETY: System::create_sound requires the first parameter to be a &str,
+        // which is immediately converted back to a Vec<u8> in the CString constructor though.
+        let sound = unsafe {
+            #[allow(clippy::transmute_bytes_to_str)] // these bytes might not be valid UTF-8
+            let bytes = mem::transmute(bytes.as_ref());
+            self.system.create_sound(bytes, mode, None)?
+        };
+
+        // Calculate start/end points
+        // TODO: figure out points with speed factor
+        let (sample_rate, _, _) = self.system.get_software_format()?;
+
+        let start_point = AudioSettings::millis_to_pcm(settings.start, sample_rate);
+
+        let end_point = match settings.end > 0 {
+            true => AudioSettings::millis_to_pcm(settings.end, sample_rate),
+            false => sound.get_length(TimeUnit::PCM)?,
+        };
+
+        // Prevent invalid parameters from being passed at all
+        if settings.volume == 0.0 || start_point >= end_point { return Ok(()) }
+
+        // Start/end points for looped sound
+        if settings.looped {
+            sound.set_loop_points(start_point, TimeUnit::PCM, end_point, TimeUnit::PCM)?;
         }
-    }
-}
 
-// pub visibility required by lazy_static
-pub struct Channel<T> {
-    sender: Sender<T>,
-    receiver: Receiver<T>,
-}
+        let channel = self.system.play_sound(sound, None, false)?;
 
-impl<T> From<(Sender<T>, Receiver<T>)> for Channel<T> {
-    fn from((sender, receiver): (Sender<T>, Receiver<T>)) -> Self {
-        Self { sender, receiver }
-    }
-}
+        // Prepare start offset and fade in
+        // TODO: figure out overlapping fade times, and speed factor
+        channel.set_position(start_point, TimeUnit::PCM)?;
+        channel.add_fade_point(0, 0.0)?;
 
-lazy_static! {
-    static ref PLAYERS: Arc<Mutex<usize>> = Default::default();
-    static ref AUDIO_MESSAGES: Channel<Instant> = crossbeam_channel::unbounded().into();
-}
+        let fade_in_time = AudioSettings::millis_to_pcm(settings.fade_in, sample_rate) as u64;
 
-pub fn play_sound(ogg: Vec<u8>, settings: AudioSettings) {
-    *PLAYERS.lock() += 1;
-    let start_time = Instant::now();
+        // Prepare end point and fade out for single sound
+        // TODO: figure out overlapping fade times, and speed factor
+        let duration = (end_point - start_point) as u64;
+        if !settings.looped {
+            channel.add_fade_point(duration, 0.0)?;
+            channel.set_delay(None, Some(duration), true)?;
+        }
 
-    if settings.pitch != 0.0 || settings.speed != 1.0 {
-        let pitch_correction = 2_f32.powf(settings.pitch / 12.0);
-        let speed_correction = 1.0 / 2_f32.powf(settings.speed / 12.0);
-        let pitch_correction = pitch_correction / speed_correction;
+        let fade_out_time = AudioSettings::millis_to_pcm(settings.fade_out, sample_rate) as u64;
 
-        let mut mixer = Mixer::new();
-        mixer.init();
+        // Pitch shift
+        let pitch = AudioSettings::linear_to_exp(settings.pitch);
+        let pitch_shift = self.system.create_dsp_by_type(DspType::Pitchshift)?;
+        pitch_shift.set_parameter_float(DspPitchShift::Pitch.into(), pitch)?;
+        channel.add_dsp(ChannelControlDspIndex::Tail.into(), pitch_shift)?;
 
-        // println!("{:?}", bot::AudioSegment::from_bytes(ogg.clone()));
+        *self.channel.write() = Some(channel);
 
-        if let Ok(mut audio_segment) = bot::AudioSegment::from_bytes(ogg) {
-            let initial_sample_rate = audio_segment.sample_rate;
-            let new_rate = (initial_sample_rate as f32 * pitch_correction) as u32;
+        let channel = self.channel.clone();
+        let settings = self.settings.clone();
 
-            audio_segment.resample(new_rate);
-            audio_segment.set_volume(settings.volume);
+        thread::spawn(move || -> Result<()> {
+            loop {
+                let Some(channel) = *channel.read() else { break };
+                if !channel.is_playing()? { break }
 
-            let frames = audio_segment.frames.iter().map(|frame| {
-                Frame::new(frame.left, frame.right)
-            })
-            .collect::<Vec<Frame>>();
+                let settings = *settings.read();
 
-            let sound = Sound::from_frames(initial_sample_rate, &frames);
+                if !settings.looped {
+                    // If looping is disabled, let the current iteration finish
+                    channel.set_loop_count(0)?;
+                }
 
-            let sound_handle = mixer.play(sound.clone());
+                channel.set_volume(settings.volume)?;
 
-            while !mixer.is_finished() {
-                if let Ok(received_time) = AUDIO_MESSAGES.receiver.try_recv() {
-                    if received_time > start_time {
-                        sound_handle.pause();
-                        break;
-                    }
+                // This pitch-setting function also stretches time
+                channel.set_pitch(AudioSettings::linear_to_exp(settings.speed))?;
+
+                // Update fade in/out points with actual volume
+                channel.add_fade_point(fade_in_time, settings.volume)?;
+                if mode != Mode::LOOP_NORMAL { // Initially looped sounds shouldn't fade out
+                    channel.add_fade_point(duration - fade_out_time, settings.volume)?;
                 }
             }
-        }
-    } else {
-        let (_stream, handle) = OutputStream::try_default().unwrap();
-        let sink = Sink::try_new(&handle).unwrap();
-        let sfx_data = Decoder::new(Cursor::new(ogg)).unwrap();
-        sink.set_volume(settings.volume);
-        sink.append(sfx_data);
 
-        while !sink.empty() {
-            if let Ok(received_time) = AUDIO_MESSAGES.receiver.try_recv() {
-                if received_time > start_time {
-                    sink.stop();
-                    break;
-                }
-            }
-        }
+            Ok(())
+        });
+
+        Ok(())
     }
 
-    *PLAYERS.lock() -= 1;
-}
+    pub fn is_playing(&self) -> bool {
+        self.channel.read()
+            .and_then(|channel| channel.is_playing().ok())
+            .unwrap_or(false)
+    }
 
-pub fn is_playing_audio() -> bool {
-    *PLAYERS.lock() > 0
-}
-
-pub fn stop_all() {
-    for _ in 0..*PLAYERS.lock() {
-        AUDIO_MESSAGES.sender.send(Instant::now()).unwrap();
+    pub fn stop_audio(&mut self) -> Result<()> {
+        match self.channel.write().take() {
+            Some(channel) => channel.stop(),
+            None => Ok(()),
+        }
     }
 }
 
-#[derive(Clone, Debug, Default)]
-pub struct AudioSegment {
-    pub sample_rate: u32,
-    /// Interleaved channel data. Always [`AudioSegment::NUM_CHANNELS`] channels.
-    pub frames: Vec<Frame>,
-    pub pitch_table: Vec<AudioSegment>,
+impl Drop for AudioSystem {
+    fn drop(&mut self) {
+        let _ = self.system.release();
+    }
 }
 
-impl AudioSegment {
-    pub fn resample(&mut self, rate: u32) -> &mut Self {
-        let mut fractional_position = 0.0f64;
-        let mut iter = self.frames.iter();
-        let mut frames = [Frame::ZERO; 4]; // prev, cur, next, next next
-        macro_rules! push_frame {
-            ($frame:expr) => {
-                for i in 0..frames.len() - 1 {
-                    frames[i] = frames[i + 1];
-                }
-                frames[frames.len() - 1] = $frame;
-            };
-        }
+impl AudioSettings {
+    fn linear_to_exp(num: i32) -> f32 {
+        2.0_f32.powf(num as f32 / 12.0)
+    }
 
-        // fill resampler with 3 frames
-        for _ in 0..3 {
-            push_frame!(iter.next().copied().unwrap_or(Frame::ZERO));
-        }
-
-        let mut resampled_frames = Vec::with_capacity(self.frames.len());
-        let dt = rate as f64 / self.sample_rate as f64;
-
-        'outer: loop {
-            resampled_frames.push(interpolate_frame(
-                frames[0],
-                frames[1],
-                frames[2],
-                frames[3],
-                fractional_position as f32,
-            ));
-
-            fractional_position += dt;
-            while fractional_position >= 1.0 {
-                fractional_position -= 1.0;
-                let Some(frame) = iter.next().copied() else {
-                    break 'outer;
-                };
-                push_frame!(frame);
-            }
-        }
-
-        self.sample_rate = rate;
-        self.frames = resampled_frames;
-        self
+    fn millis_to_pcm(millis: u32, sample_rate: i32) -> u32 {
+        (millis as f32 / 1000.0 * sample_rate.unsigned_abs() as f32) as u32
     }
 }
