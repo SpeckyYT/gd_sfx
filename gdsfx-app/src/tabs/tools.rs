5d2f2ae5c5569c91902b87b757882dcc0eea3df3
SpeckyYT <specky.fans@gmail.com>
2025-02-21 23:57:30 +0100

    updated to egui 0.31.0

--- gdsfx-app/src/tabs/tools.rs
+++ gdsfx-app/src/tabs/tools.rs
@@ -1,8 +1,10 @@
-use eframe::{egui::{Ui, Context, Slider, Layout}, emath::Align};
-use egui_modal::Modal;
+use eframe::{egui::{mutex::Mutex, Context, Layout, Modal, Sides, Slider, Ui}, emath::Align};
 use gdsfx_library::{FileEntry, MusicFileEntry, MusicLibrary, SfxFileEntry, SfxLibrary};
+use once_cell::sync::Lazy;
 
-use crate::{backend::{AppState, LibraryPage}, i18n::LocalizedEnum, layout};
+use crate::{backend::{AppState, LibraryPage}, i18n::LocalizedEnum, layout::{self, add_caution_button}};
+
+static DOWNLOAD_MODAL_OPEN: Lazy<Mutex<bool>> = Lazy::new(|| Mutex::new(false));
 
 pub fn render(ui: &mut Ui, ctx: &Context, app_state: &mut AppState, sfx_library: &SfxLibrary, music_library: &MusicLibrary) {
     layout::add_library_page_selection(ui, app_state);
@@ -16,7 +18,10 @@ pub fn render(ui: &mut Ui, ctx: &Context, app_state: &mut AppState, sfx_library:
     ui.add_space(10.0);
 
     let is_tool_running = app_state.is_tool_running();
-    let download_select_range_modal = download_range_select_modal(ctx, app_state);
+
+    if *DOWNLOAD_MODAL_OPEN.lock() {
+        download_range_select_modal(ctx, app_state);
+    }
 
     ui.add_enabled_ui(!is_tool_running, |ui| {
         let download_all_key = format!("tools.download_all.{}", app_state.library_page.localization_key());
@@ -36,7 +41,7 @@ pub fn render(ui: &mut Ui, ctx: &Context, app_state: &mut AppState, sfx_library:
             }
         }
         if ui.button(t!("tools.download_from_range")).clicked() {
-            download_select_range_modal.open();
+            *DOWNLOAD_MODAL_OPEN.lock() = true;
         }
     });
 
@@ -66,57 +71,60 @@ fn render_running_tool(ui: &mut Ui, app_state: &mut AppState) {
     }
 }
 
-fn download_range_select_modal(ctx: &Context, app_state: &mut AppState) -> Modal {
-    let modal = Modal::new(ctx, "download_range_select");
+fn download_range_select_modal(ctx: &Context, app_state: &mut AppState) {
+    let modal = Modal::new("download_range_select".into()).show(ctx, |ui| {
+        ui.heading(t!("tools.download_from_range"));
 
-    modal.show(|ui| {
-        modal.title(ui, t!("tools.download_from_range"));
+        let (min_id_range, max_id_range, range) = match app_state.library_page {
+            LibraryPage::Sfx => (0, 200000, &mut app_state.download_id_range_sfx),
+            LibraryPage::Music => (10000000, 10100000, &mut app_state.download_id_range_music),
+        };
 
-        modal.frame(ui, |ui| {
-            let (min_id_range, max_id_range, range) = match app_state.library_page {
-                LibraryPage::Sfx => (0, 200000, &mut app_state.download_id_range_sfx),
-                LibraryPage::Music => (10000000, 10100000, &mut app_state.download_id_range_music),
-            };
+        let from_slider = Slider::new(&mut range.0, min_id_range..=max_id_range)
+            .text(t!("tools.download_from_range.from_id"));
 
-            let from_slider = Slider::new(&mut range.0, min_id_range..=max_id_range)
-                .text(t!("tools.download_from_range.from_id"));
+        ui.add(from_slider);
+        range.1 = range.1.max(range.0);
 
-            ui.add(from_slider);
-            range.1 = range.1.max(range.0);
+        ui.add_space(10.0);
 
-            ui.add_space(10.0);
+        let to_slider = Slider::new(&mut range.1, min_id_range..=max_id_range)
+            .text(t!("tools.download_from_range.to_id"));
 
-            let to_slider = Slider::new(&mut range.1, min_id_range..=max_id_range)
-                .text(t!("tools.download_from_range.to_id"));
+        ui.add(to_slider);
+        range.0 = range.0.min(range.1);
 
-            ui.add(to_slider);
-            range.0 = range.0.min(range.1);
-        });
-
-        modal.buttons(ui, |ui| {
-            if ui.button(t!("tools.confirm")).triple_clicked() {
-                let download_from_range_string = "tools.download_from_range".to_string();
-                match app_state.library_page {
-                    LibraryPage::Sfx => {
-                        let range = app_state.download_id_range_sfx;
-                        app_state.download_multiple_sfx(
-                            download_from_range_string,
-                            (range.0..=range.1).map(SfxFileEntry::new).collect()
-                        );
-                    },
-                    LibraryPage::Music => {
-                        let range = app_state.download_id_range_music;
-                        app_state.download_multiple_sfx(
-                            download_from_range_string,
-                            (range.0..=range.1).map(MusicFileEntry::new).collect()
-                        );
+        Sides::new().show(
+            ui,
+            |_ui| {},
+            |ui| {
+                if ui.button(t!("tools.confirm")).triple_clicked() {
+                    let download_from_range_string = "tools.download_from_range".to_string();
+                    match app_state.library_page {
+                        LibraryPage::Sfx => {
+                            let range = app_state.download_id_range_sfx;
+                            app_state.download_multiple_sfx(
+                                download_from_range_string,
+                                (range.0..=range.1).map(SfxFileEntry::new).collect()
+                            );
+                        },
+                        LibraryPage::Music => {
+                            let range = app_state.download_id_range_music;
+                            app_state.download_multiple_sfx(
+                                download_from_range_string,
+                                (range.0..=range.1).map(MusicFileEntry::new).collect()
+                            );
+                        }
                     }
+                    *DOWNLOAD_MODAL_OPEN.lock() = false;
                 }
-                modal.close();
+                if add_caution_button(ui,t!("tools.cancel")).clicked() {
+                    *DOWNLOAD_MODAL_OPEN.lock() = false;
+                }
             }
-            modal.caution_button(ui, t!("tools.cancel"));
-        })
+        );
     });
-
-    modal
+    if modal.should_close() {
+        *DOWNLOAD_MODAL_OPEN.lock() = false;
+    }
 }
